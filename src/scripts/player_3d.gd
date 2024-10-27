3266a9fa3c871a2f0257cb6c60937e05e7f283f2
Austin Nhung <austinnhung@yahoo.ca>
2024-10-27 02:22:36 -0600

    Update player code to take the finished drink

--- src/scripts/player_3d.gd
+++ src/scripts/player_3d.gd
@@ -25,7 +25,13 @@ func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("interact"):
 		if self._current_station != null:
 			if self._inventory.get_child_count() > 0:
-				self._current_station.add_inventory(self._inventory.get_child(0))
+				var item: InventoryItem = self._current_station.add_inventory(
+					self._inventory.get_child(0),
+				)
+
+				if item != null:
+					item.reparent(self._inventory)
+				print(self._inventory.get_child_count())
 			elif self._current_station.has_inventory():
 				var item: InventoryItem = self._current_station.get_inventory()
 
