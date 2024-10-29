64f205e2a81c005618e28e7ae804a346ee676555
Austin Nhung <austinnhung@yahoo.ca>
2024-10-29 02:00:45 -0600

    Create customer manager that spawn and despawns customers

---
+++ src/scripts/customer_manager.gd
@@ -0,0 +1,35 @@
+class_name CustomerManager
+extends Node3D
+
+
+@onready var _despawn_area := %DespawnArea
+@onready var _spawn_point := %SpawnPoint
+@onready var _timer := $Timer
+@onready var _vending_machine_manager := %VendingMachineManager
+
+
+var _customer_3d := preload("res://scenes/customer_3d.tscn")
+
+
+func _ready() -> void:
+	assert(
+		self._vending_machine_manager != null,
+		"Scene is missing a VendingMachineManager!",
+	)
+
+	self._despawn_area.body_entered.connect(self._on_despawn_area_body_entered)
+	self._timer.timeout.connect(self._on_timer_timeout)
+
+
+func _on_despawn_area_body_entered(body: Node3D) -> void:
+	if body is Customer3D:
+		body.queue_free()
+
+
+func _on_timer_timeout() -> void:
+	print("Spawning Customer")
+	var customer: Customer3D = self._customer_3d.instantiate()
+
+	self.add_child(customer)
+	customer.position = self._spawn_point.position
+	customer.target_position = self._vending_machine_manager.get_random_queue_position()
