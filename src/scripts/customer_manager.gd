a354ca35b6def295be6084201eb9461f8ef32fd4
Austin Nhung <austinnhung@yahoo.ca>
2024-11-13 01:01:46 -0700

    Fix customer avoidance so that they line up straight

--- src/scripts/customer_manager.gd
+++ src/scripts/customer_manager.gd
@@ -13,6 +13,8 @@ signal customer_order_fulfilled(customer_id: int)
 
 
 var _customer_3d := preload("res://scenes/customer_3d.tscn")
+var _customers: Dictionary = {}
+var _queues: Dictionary = {}
 
 
 func _ready() -> void:
@@ -25,6 +27,36 @@ func _ready() -> void:
 	self._timer.timeout.connect(self._on_timer_timeout)
 
 
+func __spawn_customer() -> void:
+	var customer: Customer3D = self._customer_3d.instantiate()
+	var customer_id: int = customer.get_instance_id()
+	var target: VendingMachine = self._vending_machine_manager.get_random()
+	var target_id: int = target.get_instance_id()
+
+	self.add_child(customer)
+	self._customers[customer_id] = customer
+	self._queues.get_or_add(target_id, []).append(customer_id)
+
+	customer.position = self._spawn_point.position
+	print(target.global_position, target.queue_position)
+	customer.target_position = target.global_position + (target.queue_position * len(self._queues[target_id]))
+	customer.order_fulfilled.connect(self._on_customer_order_fulfilled)
+
+	self.customer_spawned.emit(customer)
+
+
+func __update_customer_target_positions() -> void:
+	for vending_machine_id: int in self._queues:
+		var vending_machine: VendingMachine = self._vending_machine_manager.get_by_id(vending_machine_id)
+		var queue_position: int = 1
+
+		for customer_id: int in self._queues[vending_machine_id]:
+			var customer: Customer3D = self._customers[customer_id]
+
+			customer.target_position = vending_machine.global_position + (vending_machine.queue_position * queue_position)
+			queue_position += 1
+
+
 func _on_despawn_area_body_entered(body: Node3D) -> void:
 	if body is Customer3D:
 		body.queue_free()
@@ -32,15 +64,16 @@ func _on_despawn_area_body_entered(body: Node3D) -> void:
 
 func _on_customer_order_fulfilled(customer: Customer3D) -> void:
 	customer.target_position = self._despawn_area.global_position
+
+	for customer_array: Array in self._queues.values():
+		if customer_array[0] == customer.get_instance_id():
+			customer_array.pop_front()
+			break
+
+	self.__update_customer_target_positions()
+
 	self.customer_order_fulfilled.emit(customer.get_instance_id())
 
 
 func _on_timer_timeout() -> void:
-	var customer: Customer3D = self._customer_3d.instantiate()
-
-	self.add_child(customer)
-	customer.position = self._spawn_point.position
-	customer.target_position = self._vending_machine_manager.get_random_queue_position()
-	customer.order_fulfilled.connect(self._on_customer_order_fulfilled)
-
-	self.customer_spawned.emit(customer)
+	self.__spawn_customer()
