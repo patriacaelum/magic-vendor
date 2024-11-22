237103e87a579fc10a12e5c2cae2535b5fa2b03f
lincolnbeepboop <lincolnphung@hotmail.com>
2024-11-21 19:00:20 -0700

    Added basic animations to player

--- src/scripts/player_3d.gd
+++ src/scripts/player_3d.gd
@@ -13,7 +13,7 @@ extends CharacterBody3D
 @onready var _inventory := %Inventory
 @onready var _item_marker := %ItemMarker
 @onready var _front_raycast_3d := %FrontRayCast
-
+@onready var _octo_skin_3d: OctoSkin3D = %OctoSkin3D
 
 const GRAVITY: Vector3 = 40.0 * Vector3.DOWN
 
@@ -120,3 +120,9 @@ func __move(delta: float) -> void:
 
 	self.velocity += (steering_vector * steering_amount) + (self.GRAVITY * delta)
 	move_and_slide()
+	
+	# Update the skin animation based on movement.
+	if is_on_floor() and not direction.is_zero_approx():
+		_octo_skin_3d.walk()
+	else:
+		_octo_skin_3d.idle()
