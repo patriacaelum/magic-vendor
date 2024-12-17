2e85c5818d12d13eb3ee18fc4e9764c9836812d0
Austin Nhung <austinnhung@yahoo.ca>
2024-12-17 02:00:02 -0700

    Add walking sound to when player walks

--- src/scripts/states/player_walk_state.gd
+++ src/scripts/states/player_walk_state.gd
@@ -2,8 +2,47 @@ class_name PlayerWalkState
 extends BasePlayerState
 
 
+@export var audio_random_pitch := 0.0
+@export var audio_random_volume := 0.0
+@export var sfx: Array[AudioStream]
+@export_range(0.1, 1.0, 0.05) var step_time: float = 0.5
+@export_range(-5, 5, 0.1) var volume: float = 0.0
+
+var _audio_player := AudioStreamPlayer.new()
+var _step_timer := Timer.new()
+
+
 func _init() -> void:
     self.state_name = STATENAME.WALK
+
+
+func _ready() -> void:
+    super()
+
+    var sfx_randomizer := AudioStreamRandomizer.new()
+    sfx_randomizer.random_pitch += self.audio_random_pitch
+    sfx_randomizer.random_volume_offset_db += self.audio_random_volume
+
+    for sfx_: AudioStream in self.sfx:
+        sfx_randomizer.add_stream(-1, sfx_)
+
+    self._audio_player.stream = sfx_randomizer
+    self._audio_player.bus = GLOBALS.AUDIO_BUS_SFX
+    self._audio_player.volume_db = self.volume
+    self.add_child(self._audio_player)
+
+    self._step_timer.timeout.connect(self._on_step_timer_timeout)
+    self.add_child(self._step_timer)
+
+
+func enter() -> void:
+    super()
+    self._step_timer.start(self.step_time)
+
+
+func exit() -> void:
+    self._step_timer.stop()
+    super()
 
 
 func notify(event: InputEvent) -> void:
@@ -43,3 +82,7 @@ func update(delta: float) -> void:
     self.__move(delta, direction)
     self.__face_mouse()
     self._entity.check_front()
+
+
+func _on_step_timer_timeout() -> void:
+    self._audio_player.play()
